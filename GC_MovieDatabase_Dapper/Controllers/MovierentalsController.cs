0438faa422c14c80844b3846cecfcea512f3e573
Ryan Raczak <ryanraczak@rocketmortgage.com>
2021-12-07 17:19:58 -0500

    initial commit

---
+++ GC_MovieDatabase_Dapper/Controllers/MovierentalsController.cs
@@ -0,0 +1,123 @@
+﻿using Dapper;
+using GC_MovieDatabase_Dapper.Models;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GC_MovieDatabase_Dapper.Controllers
+{
+    public class MovierentalsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registration(Movie m)
+        {
+            Movie movie = m;
+            if (ModelState.IsValid)
+            {
+                using (var connect = new MySqlConnection(Secret.Connection))
+                {
+                    string sql = $"insert into movies values({0},'{movie.Title}','{movie.Category}',{movie.Year},{movie.Runtime})";
+
+                    connect.Open();
+
+                    connect.Query(sql);
+
+                    connect.Close();
+
+                    return RedirectToAction("Result", m);
+                }
+                
+            }
+            else
+            {
+                return View();
+            }            
+        }
+
+        public IActionResult Result(Movie m)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(m);
+            }
+            else
+            {
+                return RedirectToAction("Registration");
+            }
+            
+        }
+
+        public IActionResult ListMovies()
+        {
+            List<Movie> movies = new List<Movie>();
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                string sql = "select * from movies";
+
+                connect.Open();
+
+                movies = connect.Query<Movie>(sql).ToList();
+
+                connect.Close();
+            }
+            return View(movies);
+        }
+
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SearchByTitle(String s)
+        {
+            List<Movie> movies = new List<Movie>();
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                string sql = $"select * from movies where Title = '{s}'";
+
+                connect.Open();
+
+                movies = connect.Query<Movie>(sql).ToList();
+
+                connect.Close();
+                return RedirectToAction("SearchResult", movies);
+            }
+        }
+        [HttpPost]
+        public IActionResult SearchByGenre(String s)
+        {
+            List<Movie> movies = new List<Movie>();
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                string sql = $"select * from movies where Genre = '{s}'";
+
+                connect.Open();
+
+                movies = connect.Query<Movie>(sql).ToList();
+
+                connect.Close();
+               
+            }
+            return RedirectToAction("SearchResult", movies);
+        }
+
+        public IActionResult SearchResult(List<Movie>movies)
+        {
+            return View(movies);
+        }
+    }
+}
---
+++ GC_MovieDatabase_Dapper/Models/Movie.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GC_MovieDatabase_Dapper.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [MinLength(1, ErrorMessage ="Title must have at least 1 character")]
+        [MaxLength(30, ErrorMessage ="Title can be no longer then 30 characters.")]
+        public string Title { get; set; }
+
+        [Required]
+        public Genre Category { get; set; }
+
+        [Required]
+        [Range(1880,2021, ErrorMessage ="Must be between year 1880 and 2021")]
+        public int Year { get; set; }
+
+        [Required]
+        public int Runtime { get; set; }
+
+    }
+    public enum Genre
+    {
+        Animation,
+        Horror,
+        Drama,
+        Action,
+        Comedy
+    }
+}
